9bf42813a660ef0a8f618486fa3602facc29a509
Unknown <admin@lucidlynx.net>
2017-03-23 16:31:06 -0400

    Format Code

--- src/luaScanner.java
+++ src/luaScanner.java
@@ -10,8 +10,7 @@ import java.util.List;
  */
 public class luaScanner {
 
-    public luaScanner()
-    {
+    public luaScanner() {
     }
 
     public void processFile(String fileName) {
@@ -20,8 +19,7 @@ public class luaScanner {
         try {
             String line;
             BufferedReader reader = new BufferedReader(new FileReader(fileName));
-            while((line = reader.readLine()) != null)
-            {
+            while ((line = reader.readLine()) != null) {
                 stringList.add(line);
             }
         } catch (FileNotFoundException e) {
@@ -31,146 +29,80 @@ public class luaScanner {
             e.printStackTrace();
         }
 
-        for(int i = 0; i < stringList.size(); i++)
-        {
+        for (int i = 0; i < stringList.size(); i++) {
             String[] slices = stringList.get(i).split(" ");
-            for(int j = 0; j < slices.length; j++)
-            {
-                if(slices[j].startsWith("(") && slices[j].length() > 1)
-                {
-                    System.out.println(slices[j].substring(0,1)+" : Open Parentheses");
-                    System.out.println(slices[j].substring(1) + " : " +getType(slices[j].substring(1)));
-                }
-                else if(slices[j].endsWith(")"))
-                {
-                    if(getType(slices[j]).equals("Function ID"))
-                    {
+            for (int j = 0; j < slices.length; j++) {
+                if (slices[j].startsWith("(") && slices[j].length() > 1) {
+                    System.out.println(slices[j].substring(0, 1) + " : Open Parentheses");
+                    System.out.println(slices[j].substring(1) + " : " + getType(slices[j].substring(1)));
+                } else if (slices[j].endsWith(")")) {
+                    if (getType(slices[j]).equals("Function ID")) {
 
-                    }
-                    else
-                    {
-                        System.out.println(slices[j].substring(0,1) + " : " +getType(slices[j].substring(0,1)));
-                        System.out.println(slices[j].substring(slices[j].length()-1) + ": Close Parentheses");
+                    } else {
+                        System.out.println(slices[j].substring(0, 1) + " : " + getType(slices[j].substring(0, 1)));
+                        System.out.println(slices[j].substring(slices[j].length() - 1) + ": Close Parentheses");
                     }
 
-                }
-                else
-                {
-                    System.out.println(slices[j] + " : " +getType(slices[j]));
+                } else {
+                    System.out.println(slices[j] + " : " + getType(slices[j]));
                 }
 
             }
         }
     }
 
-    public String getType(String input)
-    {
-        if(input.matches("[A-Za-z]\\(\\)"))
-        {
+    public String getType(String input) {
+        if (input.matches("[A-Za-z]\\(\\)")) {
             return "Function ID";
-        }
-        else if(input.equals("function"))
-        {
+        } else if (input.equals("function")) {
             return "Function Keyword";
-        }
-        else if(input.equals("end"))
-        {
+        } else if (input.equals("end")) {
             return "End Keyword";
-        }
-        else if(input.equals("="))
-        {
+        } else if (input.equals("=")) {
             return "Assignment Operator";
-        }
-        else if(input.equals("<="))
-        {
+        } else if (input.equals("<=")) {
             return "LE Operator";
-        }
-        else if(input.equals("<"))
-        {
+        } else if (input.equals("<")) {
             return "LT Operator";
-        }
-        else if(input.equals(">="))
-        {
+        } else if (input.equals(">=")) {
             return "GE Operator";
-        }
-        else if(input.equals(">"))
-        {
+        } else if (input.equals(">")) {
             return "GT Operator";
-        }
-        else if(input.equals("=="))
-        {
+        } else if (input.equals("==")) {
             return "EQ Operator";
-        }
-        else if(input.equals("~="))
-        {
+        } else if (input.equals("~=")) {
             return "NE Operator";
-        }
-        else if(input.equals("+"))
-        {
+        } else if (input.equals("+")) {
             return "Add Operator";
-        }
-        else if(input.equals("-"))
-        {
+        } else if (input.equals("-")) {
             return "Sub Operator";
-        }
-        else if(input.equals("*"))
-        {
+        } else if (input.equals("*")) {
             return "Mult Operator";
-        }
-        else if(input.equals("/"))
-        {
+        } else if (input.equals("/")) {
             return "Div Operator";
-        }
-        else if(input.matches("\\d+"))
-        {
+        } else if (input.matches("\\d+")) {
             return "Int Literal";
-        }
-        else if(input.matches("[A-Za-z]"))
-        {
+        } else if (input.matches("[A-Za-z]")) {
             return "String literal";
-        }
-        else if(input.equals("("))
-        {
+        } else if (input.equals("(")) {
             return "Open Parentheses";
-        }
-
-        else if(input.equals(")"))
-        {
+        } else if (input.equals(")")) {
             return "Close Parentheses";
-        }
-
-        else if(input.equals("print"))
-        {
+        } else if (input.equals("print")) {
             return "Print Statement";
-        }
-
-        else if(input.equals("if"))
-        {
+        } else if (input.equals("if")) {
             return "If Statement";
-        }
-
-        else if(input.equals("then"))
-        {
+        } else if (input.equals("then")) {
             return "Then Statement";
-        }
-        else if(input.equals("else"))
-        {
+        } else if (input.equals("else")) {
             return "Else Statement";
-        }
-        else if(input.equals("while"))
-        {
+        } else if (input.equals("while")) {
             return "While Statement";
-        }
-        else if(input.equals("do"))
-        {
+        } else if (input.equals("do")) {
             return "Do Statement";
-        }
-        else if(input.equals("repeat"))
-        {
+        } else if (input.equals("repeat")) {
             return "Repeat Statement";
-        }
-        else if(input.equals("until"))
-        {
+        } else if (input.equals("until")) {
             return "Until Statement";
         }
         return "Unknown Token";
